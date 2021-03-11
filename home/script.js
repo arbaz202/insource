474e7b46bb3e0a6b574cd791b872cf2cd249deef
arbaz khan <arbazkhan202@gmail.com>
2021-03-11 14:07:35 +0530

    first commit

---
+++ home/script.js
@@ -0,0 +1,6 @@
+console.log('hii')
+   
+    window.addEventListener("scroll",function(){
+      var header = document.querySelector("header");
+      header.classList.toggle("sticky",window.scrollY > 0)
+    })
